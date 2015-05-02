c06b50f9701310bcf3cddbb02a501494c5dc3df1
Christian Gossain <christian@showbie.com>
2015-05-02 15:53:53 -0600

    Cleaner and more stable Arduino RFID reader implementation.
    The Arduino code for the RFID reader has been cleaned up by removing unnecessary variables, removing dependance on the `delay()` function, adding convenience functions to create LED blink patterns, etc.
    
    This rewrite also introduces the watchdog timer to reset the device whenever it freezes (i.e. due to a bad network connection, etc).

--- hardware/arduino_bat_scanner/bat_Scanner.ino
+++ hardware/arduino_bat_scanner/bat_Scanner.ino
@@ -1,254 +1,433 @@
 #include <Adafruit_CC3000.h>
-#include <SPI.h>
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <avr/wdt.h>
 
-int tableID = 1;
-int wifiPin = 6;
-int readLed = 7;
-long previousMillis = 0; 
-byte bytesread = 0;
-long interval = 5000; 
-boolean nodeConnected = false;
+int tableID = 1; 
+int redLed = 6;
+int blueLed = 5;
 
 // Digital pins
-#define RESET_PIN         4
 #define RFID_RX           9
 #define RFID_TX           8 // not used
-#define RESET_LED_PIN     13
+
 SoftwareSerial rfidSerial(RFID_RX, RFID_TX);
 
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
-// These can be any two pins
-#define ADAFRUIT_CC3000_VBAT  5
-#define ADAFRUIT_CC3000_CS    10
-// Use hardware SPI for the remaining pins
+// CC3000 interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   2 // MUST be an interrupt pin!
+#define ADAFRUIT_CC3000_VBAT  7 // These can be
+#define ADAFRUIT_CC3000_CS   10 // any two pins
+
+// Hardware SPI required for remaining pins.
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
-Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER); 
-Adafruit_CC3000_Client www;
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS,
+ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIV2);
 
-#define WLAN_SSID       "Something"
-#define WLAN_PASS       "featuredInDotNet100"
-
-// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
-#define WLAN_SECURITY   WLAN_SEC_WPA2
+// WiFi access point credentials
+#define WLAN_SSID     "YOUR_SSID"  // 32 characters max
+#define WLAN_PASS     "YOUR_PASSWORD"
+#define WLAN_SECURITY WLAN_SEC_WPA2 // WLAN_SEC_UNSEC/WLAN_SEC_WEP/WLAN_SEC_WPA/WLAN_SEC_WPA2
 
 // IP and port of netServer we are sending events to
-const uint8_t   SERVER_IP[4]   = { 
-  0, 0, 0, 0 };
-const uint16_t  SERVER_PORT    = 0000;
+const uint8_t   
+  SERVER_IP[4] = { 0, 0, 0, 0 };
+  
+const uint16_t  
+  SERVER_PORT = 0000;
+  
+// Intervals
+const unsigned long
+  alivePingInterval = 15L * 1000L; // Time to wait before sending another alive packet
+  
+unsigned long previousAlivePingMillis = 0;
 
-/* Amount of time to wait (in milliseconds) with no data 
- received before closing the connection.  If you know the server
- you're accessing is quick to respond, you can reduce this value.                            
- software serial connection for rfid reader */
-#define IDLE_TIMEOUT_MS  30000  
-
-void setup(void)
-{
+void setup() {
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // disable watchdog
+  wdt_disable();
+  
   // Setup serial monitor for debugging
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  
+  pinMode(redLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  
+  digitalWrite(redLed, LOW);
+  digitalWrite(blueLed, LOW);
+  
+  delay(1000);
 
-  // Setup serial connection to RFID reader
-  rfidSerial.begin(9600);
-
-  pinMode(wifiPin, OUTPUT);
-  pinMode(readLed, OUTPUT);
-
-  digitalWrite(readLed, HIGH);
-  digitalWrite(wifiPin, HIGH);
-
-  Serial.println(F("\nInitializing..."));
-
+  digitalWrite(redLed, HIGH);
+  digitalWrite(blueLed, HIGH);
+  
+  //Serial.print(F("\nInitializing CC3000..."));
+  
   if(!cc3000.begin()) {
-    Serial.println(F("Couldn't begin()! Check your wiring?"));
+    //Serial.println(F("Couldn't begin()! Check your wiring?"));
     while(1);
   }
-
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-    Serial.println(F("Failed!"));
-    while(1);
-  }
-
-  Serial.println(F("Connected to WIFI!"));
-
-  // Wait for DHCP to complete
-  Serial.println(F("Request DHCP"));
+  
+  redBlink();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  //Serial.print(F("OK\r\nConnecting to WiFi network..."));
+  cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
+  
+  redBlink();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  //Serial.print(F("OK\r\nRequesting DHCP address..."));
   while (!cc3000.checkDHCP()) {
-    delay(100); // ToDo: Insert a DHCP timeout!
-  }   
-
-  Serial.println(F("Listen for a card to be read"));
+    delay(100);
+  }
+  //Serial.println(F("OK"));
+  
+  redBlink();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // Initialize the serial connection to the RFID reader
+  rfidSerial.begin(9600);
+  //Serial.println(F("Listen for a card to be read"));
+  
+  // start watchdog 
+  enable_watchdog();
+  
+  // send an alive ping
   imAlive();
+  
 }
 
+void loop() {
+  
+  // listen for an RFID scan
+  listenForRFID();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // let the server know that we are connected each 'alive interval'
+  if((millis() - previousAlivePingMillis) > alivePingInterval) {
+    imAlive();
+  }
+  
+}
 
-void loop () {
+void listenForRFID(void) {
+  
   byte i = 0;
   byte val = 0;
   byte code[6];
   byte checksum = 0;
   byte bytesread = 0;
   byte tempbyte = 0;
-
+  
   if(rfidSerial.available() > 0) {
-
-    // Check for header 
-    if((val = rfidSerial.read()) == 2) {                  
-      bytesread = 0; 
-      digitalWrite(readLed, HIGH);
-
-      // Read 10 digit code + 2 digit checksum
-      while (bytesread < 12) {                        
+    val = rfidSerial.read();
+    //Serial.println("Read Value:");
+    //Serial.println(val);
+    if(val == 2) {                   // check for header 
+      bytesread = 0;
+      
+      ledStateDetectedRFID();        // update LED state
+      
+      while (bytesread < 12) {                        // read 10 digit code + 2 digit checksum
         if( rfidSerial.available() > 0) { 
-
           val = rfidSerial.read();
-
-          // If header or stop bytes before the 10 digit reading, stop reading
-          if((val == 0x0D)||(val == 0x0A)||(val == 0x03)||(val == 0x02)) { 
-            break;
+          if((val == 0x0D)||(val == 0x0A)||(val == 0x03)||(val == 0x02)) { // if header or stop bytes before the 10 digit reading 
+            break;                                    // stop reading
           }
 
-          // Do Ascii/Hex conversion
+          // Do Ascii/Hex conversion:
           if ((val >= '0') && (val <= '9')) {
             val = val - '0';
-          } 
-          else if ((val >= 'A') && (val <= 'F')) {
+          } else if ((val >= 'A') && (val <= 'F')) {
             val = 10 + val - 'A';
           }
 
           // Every two hex-digits, add byte to code:
           if (bytesread & 1 == 1) {
-
-            // Make some space for this hex-digit by
+            // make some space for this hex-digit by
             // shifting the previous hex-digit with 4 bits to the left:
             code[bytesread >> 1] = (val | (tempbyte << 4));
 
-            // If we're at the checksum byte,
-            if (bytesread >> 1 != 5) {                
-              // Calculate the checksum... (XOR)
-              checksum ^= code[bytesread >> 1];       
+            if (bytesread >> 1 != 5) {                // If we're at the checksum byte,
+              checksum ^= code[bytesread >> 1];       // Calculate the checksum... (XOR)
             };
-          } 
-          else {
-            // Store the first hex digit first
-            tempbyte = val;                           
+          } else {
+            tempbyte = val;                           // Store the first hex digit first...
           };
-          // Ready to read next digit
-          bytesread++;                               
+
+          bytesread++;                                // ready to read next digit
         } 
       } 
 
-      // Output to Serial
-      if (bytesread == 12) { 
-        int count = 0;
-        int i;
+      // Output to Serial:
+
+      if (bytesread == 12) {        // if 12 digit read is complete
         char* bufStr = (char*) malloc (2*sizeof(code) + 1);
         char* bufPtr = bufStr;
 
         bufPtr += sprintf(bufPtr, "tableID=%d:", tableID);
-
-        // If 12 digit read is complete
-        Serial.print("5-byte code: ");
-
-        for (i=0; i<6; i++) {
-          if (code[i] < 16) {
-            Serial.print("0");
-          }
-          Serial.print(code[i], HEX);
-
+      
+        //Serial.print("5-byte code: ");
+        for (i=0; i<5; i++) {
+          if (code[i] < 16) Serial.print("0");
+          //Serial.print(code[i], HEX);
+          //Serial.print(" ");
           bufPtr += sprintf(bufPtr, "%02X", code[i]);
-
-          Serial.print(" ");
         }
-
-        sendToServer(bufStr);
-
-        Serial.println();
-        Serial.print("Checksum: ");
-        Serial.print(code[5], HEX);
-        Serial.println(code[5] == checksum ? " -- passed." : " -- error.");
-        Serial.println();
-
-        delay(1000);
-        digitalWrite(readLed, LOW);
+        
+        sendToServer(bufStr);      // send the scanned RFID to the net server
+        
+        //Serial.println();
+        //Serial.print("Checksum: ");
+        //Serial.print(code[5], HEX);
+        //Serial.println(code[5] == checksum ? " -- passed." : " -- error.");
+        //Serial.println();
+        
       }
-
       bytesread = 0;
     }
   }
-  unsigned long currentMillis = millis();
-
-  if(currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;
-    wifiLed();
-    imAlive();
-  }
 }
 
-// Send message to netServer
 void sendToServer(char* string) {
-  flashLed();
-  if (www.connected()){       
-    www.fastrprint(string);
-    deviceReadAndSent();
+  
+  // close the serial port while we attempting to send
+  rfidSerial.end();
+  
+  // check connection to WiFi
+  //Serial.print(F("\n\nChecking WiFi connection..."));
+  if(!cc3000.checkConnected()){while(1){}}
+  //Serial.println(F("OK."));
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // send the scanned RFID string
+  Adafruit_CC3000_Client client = cc3000.connectTCP(cc3000.IP2U32(SERVER_IP[0], SERVER_IP[1], SERVER_IP[2], SERVER_IP[3]), SERVER_PORT);
+  if (client.connected()) {
+    
+    //Serial.print(F("Sending RFID scan..."));
+    
+    // send the scanned RFID
+    client.fastrprint(string);
+    
+    // indicate that the RFID was sent successfully
+    sendSuccessBlink();
+    //Serial.println(F("Success"));
+    
+  } else {
+    
+      // indicate that the RFID failed to send
+      sendFailiureBlink();
+      //Serial.println(F("Could not send RFID. Not connected!"));
+      
   }
-}
-
-// Flash red LED while connecting to the server
-void flashLed() {
-  www.close();
-  while(!www.connected()){
-
-    www = cc3000.connectTCP(cc3000.IP2U32(SERVER_IP[0], SERVER_IP[1], SERVER_IP[2], SERVER_IP[3]), SERVER_PORT);
-    digitalWrite(wifiPin, HIGH);
-    delay(500);
-    digitalWrite(wifiPin, LOW);
-    delay(500);
-
-    Serial.println(F("\nTrying to connect to server..."));
-  }
-  digitalWrite(wifiPin, HIGH);
-
+  
+  // reset watchdog
+  wdt_reset();
+   
+  // close connection and disconnect
+  //Serial.println(F("Closing connection"));
+  client.close();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // wait a few seconds before accepting another scan
+  ledStateWaiting();
+  
+  // delay for one second
+  wait(1000);
+  
+  // re-initialize the serial port
+  rfidSerial.begin(9600);
+  
+  // go back to the alive state
+  ledStateAlive();
+  
+  // reset watchdog
+  wdt_reset();
+  
 }
 
 void imAlive() {
-  www = cc3000.connectTCP(cc3000.IP2U32(SERVER_IP[0], SERVER_IP[1], SERVER_IP[2], SERVER_IP[3]), SERVER_PORT);
-  char online [25];
-  int n;
-  n = sprintf(online, "tableID=%d:online", tableID);
-  www.fastrprint(online);
-  www.close();
-
-  Serial.println(F("Sent alive packet"));
-}
-
-void wifiLed() {
-  if (!cc3000.checkConnected()) {
-    digitalWrite(readLed, HIGH);
-    delay(200);
-    digitalWrite(readLed, LOW);
-    delay(200);
-    digitalWrite(readLed, HIGH);
-    delay(200);
-    digitalWrite(readLed, LOW);
-    delay(200);
-    digitalWrite(readLed, HIGH);
-    cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
-
-  } 
-  else {
-    digitalWrite(readLed, LOW);
+  
+  // close the serial port while we attempting to send
+  rfidSerial.end();
+  
+  // check connection to WiFi
+  //Serial.print(F("\n\nChecking WiFi connection..."));
+  if(!cc3000.checkConnected()){while(1){}}
+  //Serial.println(F("OK."));
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // send the alive packet
+  Adafruit_CC3000_Client client = cc3000.connectTCP(cc3000.IP2U32(SERVER_IP[0], SERVER_IP[1], SERVER_IP[2], SERVER_IP[3]), SERVER_PORT);
+  if (client.connected()) {
+    
+    //Serial.print(F("Sending alive packet..."));
+    
+    // send an alive packet
+    char online [25];
+    int n;
+    n = sprintf(online, "tableID=%d:online", tableID);
+    client.fastrprint(online);
+    
+    // set the LED state
+    ledStateAlive();
+    //Serial.println(F("Success"));
+    
+  } else {
+    
+    // indicate that the alive packet failed to send
+    sendFailiureBlink();
+    //Serial.println(F("Could not send alive packet. Not connected!"));
+    
   }
+  
+  // reset watchdog
+  wdt_reset();
+   
+  // close connection and disconnect
+  //Serial.println(F("Closing connection"));
+  client.close();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // wait a few seconds before accepting another scan
+  ledStateWaiting();
+  
+  // delay for one second
+  wait(1000);
+  
+  // re-initialize the serial port
+  rfidSerial.begin(9600);
+  
+  // go back to the alive state
+  ledStateAlive();
+  
+  // reset watchdog
+  wdt_reset();
+  
+  // update the time that the last alive ping was sent
+  previousAlivePingMillis = millis();
+  
 }
-void deviceReadAndSent() {
-  digitalWrite(wifiPin, LOW);
-  delay(100);
-  digitalWrite(wifiPin, HIGH);
-  delay(100);
-  digitalWrite(wifiPin, LOW);
-  delay(100);
-  digitalWrite(wifiPin, HIGH);
 
-  Serial.println(F("\nSent message"));
+/*********** 
+ LED states
+************/
+void ledStateAlive() 
+{
+  digitalWrite(redLed, HIGH);
+  digitalWrite(blueLed, LOW);
+}
+
+void ledStateDetectedRFID() 
+{
+  digitalWrite(redLed, HIGH);
+  digitalWrite(blueLed, HIGH);
+}
+
+void ledStateWaiting() 
+{
+  digitalWrite(redLed, HIGH);
+  digitalWrite(blueLed, HIGH);
+}
+
+/******************
+ LED blink patterns
+*******************/
+void sendSuccessBlink() 
+{
+  blinkLed(redLed, blueLed, 4, 100, false); // success blink
+  ledStateAlive(); // restore alive state
+}
+
+void sendFailiureBlink() 
+{
+  blinkLed(redLed, blueLed, 4, 500, false); // fail blink
+  ledStateAlive();
+}
+
+void alivePingFailiureBlink() 
+{
+  blinkLed(redLed, blueLed, 6, 500, true); // fail blink
+  ledStateAlive();
+}
+
+void redBlink() 
+{
+  blinkLed(redLed, 0, 1, 200, false); // 1 blink
+}
+
+/*******
+ Helpers
+********/
+void blinkLed(int pin1, int pin2, unsigned long totalBlinks, unsigned long period, boolean alternate) 
+{  
+  unsigned long totalInterval = totalBlinks * (2L * period) + period;
+  unsigned long t = millis();
+  unsigned long previousMillis = 0;
+  int ledState = LOW;
+  
+  do {
+   if ((millis() - previousMillis) > period) {
+     // alternate the LED state each interval
+     if (ledState == LOW) {
+       ledState = HIGH;
+     }
+     else {
+       ledState = LOW;
+     }
+     
+     // turn on the requested pin
+     if (pin1 > 0) {
+       digitalWrite(pin1, ledState);
+     }
+     if (pin2 > 0) {
+       if (alternate) {
+         digitalWrite(pin2, !ledState);
+       }
+       else {
+         digitalWrite(pin2, ledState);
+       }
+     }
+     previousMillis = millis();
+   }
+  } 
+  while ((millis() - t) < totalInterval);
+}
+
+void wait(unsigned long total_delay) {
+  
+  unsigned long t = millis();
+  do {
+    // Wait; Do Nothing
+  }
+  while ((millis() - t) < total_delay);
+  
+}
+
+void enable_watchdog(void)
+{
+  cli(); // disable all interrupts
+  wdt_reset(); // reset the WDT timer
+  WDTCSR |= (1<<WDCE) | (1<<WDE); 
+  WDTCSR = (1<<WDIE) | (1<<WDE) | (1<<WDP3) | (1<<WDP2) | (1<<WDP1) | (1<<WDP0);
+  sei();
 }
