759f6d9a4e9e3658272cee0d6d141a5863fb5684
Daniel Frieling <df@plista.com>
2016-12-14 22:16:31 +0000

    drop audiosprite sound playing switch to direct audio playing

--- ui/public/components/GameComponent.js
+++ ui/public/components/GameComponent.js
@@ -3,13 +3,9 @@
  */
 'use strict';
 
-
-
 var
     React = require('react'),
     AmpersandState = require('ampersand-state'),
-    Howl = require('howler').Howl,
-    soundSprite = require('../build/sprite'),
     config = window.config,
     node = require('../js/node'),
     AdminComponent = require('./AdminComponent'),
@@ -17,10 +13,7 @@ var
     StatusComponent = require('./StatusComponent'),
     StatusIndicatorComponent = require('./StatusIndicatorComponent'),
     StatsComponent = require('./StatsComponent'),
-    soundPath = config.clientUrl + '/sounds/',
-    soundQueue = [],
-    soundsPlaying = false,
-    sounds,
+    soundPath = '/sounds/',
     PlayerModel,
     playerProps,
     player0,
@@ -61,8 +54,6 @@ var GameComponent = module.exports = React.createClass({
     componentDidMount: function() {
 
         var _this = this;
-        
-        sounds = new Howl(soundSprite);
         
         node.socket.on('game.end', _this.end);
         node.socket.on('game.score', _this.score);
@@ -110,8 +101,7 @@ var GameComponent = module.exports = React.createClass({
             playerSound = player1.name;
         }
 
-        this.queueSound(playerSound.toLowerCase() + '-to-serve');
-
+		this.playSound(playerSound.toLowerCase() + '-to-serve');
     },
     
     
@@ -151,8 +141,7 @@ var GameComponent = module.exports = React.createClass({
             playerSound = player1.name;
         }
 
-        this.queueSound('game-point-' + playerSound.toLowerCase());
-        
+        this.playSound('game-point-' + playerSound.toLowerCase());
     },
     
     
@@ -160,6 +149,7 @@ var GameComponent = module.exports = React.createClass({
     announceScore: function() {
     
         var announcement = this.state.score;
+		var _this = this;
         
         if(typeof this.state.winner === 'undefined' && announcement[0] > 0 || announcement[1] > 0) {
         
@@ -167,12 +157,12 @@ var GameComponent = module.exports = React.createClass({
             if(this.state.server == 1) {
                 announcement.reverse();
             }
-            
-            this.queueSound('' + announcement[0], -300);
-            this.queueSound('' + announcement[1]);
-            
+
+			setTimeout(function() {
+				_this.playSound('' + announcement[0]);
+	        }, 1100);
+			this.playSound('' + announcement[1]);
         }
-    
     },
     
     
@@ -193,16 +183,20 @@ var GameComponent = module.exports = React.createClass({
             playerSound = player1.name;
         }
         
-        this.clearAudioQueue();
-        sounds.play('game_end');
-        
+        this.playSound('game_end');
+
         setTimeout(function() {
-            this.queueSound(playerSound.toLowerCase + '-won-the-game');
+            _this.playSound(playerSound.toLowerCase + '-won-the-game');
         }, 900);
         
     },
     
-    
+    playSound: function(filename) {
+		var snd = new Audio(soundPath + filename + ".mp3");
+		setTimeout(function() {
+			snd.play();
+		}, snd.duration*1000);
+	},
     
     tableConnected: function() {
         this.setState({
@@ -242,67 +236,6 @@ var GameComponent = module.exports = React.createClass({
         });
     },
     
-    
-    
-    queueSound: function(sound, offset, cb) {
-        soundQueue.push({
-            name: sound,
-            offsetNext: typeof offset === 'undefined' ? 0 : offset,
-            cb: cb
-        });
-        this.playQueue();
-    },
-    
-    
-    
-    playQueue: function() {
-        
-        var
-            _this = this,
-            play;
-        
-        if(soundsPlaying) {
-            return;
-        }
-        
-        soundsPlaying = true;
-        
-        play = function() {
-            
-            var
-                sound = {},
-                duration = 0,
-                offset = 0;
-
-            if(soundQueue.length > 0) {
-                sound = soundQueue.shift();
-                duration = soundSprite.sprite[sound.name][1];
-                offset = sound.offsetNext ? duration + sound.offsetNext : duration;
-                sounds.play(sound.name);
-                setTimeout(function() {
-                    play();
-                    if(sound.cb) {
-                        sound.cb();
-                    }
-                }, offset);
-            } else {
-                soundsPlaying = false;
-            }
-
-        }
-
-        play();
-
-    },
-    
-    
-    
-    clearAudioQueue: function() {
-        soundQueue = [];
-    },
-
-
-
     reset: function() {
 
         setTimeout(function() {
