1f78e6ea1dd47acfe05757d2630c4f363d7f1000
Daniel Frieling <df@plista.com>
2016-12-19 09:28:16 +0000

    remove audiosprite from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -177,19 +177,7 @@ gulp.task('sounds', function(cb) {
             }, cb);
         }
     
-    ], function() {
-        var updateSprite = exec.bind(undefined, 'audiosprite --format howler --path build/ --output ui/public/build/sprite --export mp3 ui/public/sounds/*.mp3', cb);
-        
-        if(downloads.length > 0) {
-            return es.merge.apply(undefined, downloads).on('end', function() {
-				gutil.log("updating sprite...");
-                updateSprite();
-            });
-        }
-        
-        updateSprite();
-        
-    });
+    ]);
         
     function fetchAnnouncements(player, cb) {
         async.each(announcements, function(announcement, cb) {
